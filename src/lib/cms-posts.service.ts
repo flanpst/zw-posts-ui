27c069058fb0e8904748798be73944e253448485
jhomoreira <jho@outlook.com>
2023-05-19 19:21:51 -0300

    ajuste environment cms-posts

--- src/lib/cms-posts.service.ts
+++ src/lib/cms-posts.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@angular/core';
 import { CrudService } from './abstract/crud.service';
 import { CmsPosts } from './class/cms-posts';
 import { HttpClient } from '@angular/common/http';
+import { ENVIRONMENT } from './cms-posts.module';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class CmsPostsService extends CrudService<CmsPosts, number>{
 
   constructor(
-    @Inject('env') protected environment,
+    @Inject(ENVIRONMENT) protected environment,
     protected _http: HttpClient
   ) {
     super(environment, _http)

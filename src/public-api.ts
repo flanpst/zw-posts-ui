324a507ca5534d349414e64d7c9a9b26ab6daede
jhomoreira <jho@outlook.com>
2023-06-01 23:23:10 -0300

    adicionadas dependencias componente category public-api

--- src/public-api.ts
+++ src/public-api.ts
@@ -7,4 +7,6 @@ export * from './lib/cms-posts.module';
 export * from './lib/cms-posts-routing.module';
 export * from './lib/post-listar/post-listar.component';
 export * from './lib/post-criar/post-criar.component';
+export * from './lib/post-categoria-listar/post-categoria-listar.component';
+export * from './lib/post-categoria-criar/post-categoria-criar.component';
 
